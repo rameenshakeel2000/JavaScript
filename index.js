24b01cc21c8715a20050434b7cadb2851f1a4575
Rameen Shakeel <rameenshakeel2000@gmail.com>
2021-08-26 11:16:20 +0500

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function add() {
+    var formLength = document.getElementById("myForm").elements.length;
+    console.log(formLength);}
+   /* var x = document.createElement("INPUT");
+    x.setAttribute("id" + "5")
+    x.setAttribute("type", "number");
+    x.setAttribute("placeholder", "Enter number");
+    document.body.appendChild(x);
+  }
+  function submit() {
+    document.getElementsById("myform").submit();
+  }
